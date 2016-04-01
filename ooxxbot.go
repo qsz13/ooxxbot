ea0aa9d81c2a491473d7182b6c3e35619dff9c17
Daniel Qiu <qsz1328@gmail.com>
2016-04-01 21:51:35 +0800

    get ip

--- ooxxbot.go
+++ ooxxbot.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	fmt.Printf("Welcome to OOXXBot\n")
-	//bot := tbot.NewBot(TOKEN, &rc.ClientProxy{URL: "127.0.0.1:1080", ProxyType: rc.SOCKS5_PROXY})
+	bot := tbot.NewBot(TOKEN, &rc.ClientProxy{URL: "127.0.0.1:1080", ProxyType: rc.SOCKS5_PROXY})
 	//bot := tbot.NewBot(TOKEN, &rc.ClientProxy{ProxyType: rc.ENV_PROXY})
-	bot := tbot.NewBot(TOKEN, &rc.ClientProxy{URL: "http://proxy.phl.sap.corp:8080", ProxyType: rc.MANUAL_PROXY})
+	//bot := tbot.NewBot(TOKEN, &rc.ClientProxy{URL: "http://proxy.phl.sap.corp:8080", ProxyType: rc.MANUAL_PROXY})
 	// fmt.Println(bot.GetMe())
 	// fmt.Println(bot.GetUpdates(100, 100, 10))
 	bot.Messages = make(chan *tbot.Message, 1000)
@@ -23,6 +23,6 @@ func main() {
 func handleMessages(bot *tbot.Bot) {
 	for message := range bot.Messages {
 		fmt.Println(message.Text)
-		bot.ReplyText(message.Chat.ID, "great!")
+		bot.ExecCmd(message)
 	}
 }
--- tbot/bot.go
+++ tbot/bot.go
@@ -3,6 +3,7 @@ package tbot
 import (
 	"fmt"
 	rc "github.com/qsz13/ooxxbot/requestclient"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -54,5 +55,32 @@ func (bot *Bot) loop(messages chan *Message, queries chan *InlineQuery) {
 func (bot *Bot) ReplyText(ChatID int, Text string) {
 
 	bot.sendMessage(ChatID, Text, "", false, false, -1)
+
+}
+
+func (bot *Bot) ExecCmd(message *Message) {
+	switch message.Text {
+	case "/ip":
+		go bot.getIP(message)
+		break
+
+	}
+
+}
+
+func (bot *Bot) getIP(message *Message) {
+	res, err := http.Get("http://wtfismyip.com/text")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ip := string(body)
+	bot.ReplyText(message.Chat.ID, ip)
 
 }
